9ad4bb23964c35669800d2931948052c5c6098a1
Saad Khan <saad.khan955@gmail.com>
2023-12-08 18:53:37 +0530

    Added SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/Header'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata: Metadata = {
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
         <Header />
         {children}
       </body>
